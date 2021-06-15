85a174a4e8165089460a541a97ea55e7a75628e0
Daniel Janus <dj@danieljanus.pl>
2021-06-15 12:10:11 +0200

    Add code to manifest the bug.

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Swiper from 'react-native-swiper';
+
+const Card = (props) => {
+  return (
+    <View style={styles.card}>
+      <Text>Card {props.number}</Text>
+    </View>
+  );
+}
 
 export default function App() {
+  const [pressed, press] = React.useState(false);
+  const onPress = () => press(true);
+  const cards = [1, 2, 3].map((x) => <Card key={x} number={x} />);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity style={styles.button} onPress={onPress}>
+        <Text style={styles.buttonText}>Kaboom!</Text>
+      </TouchableOpacity>
+      {
+        React.createElement(Swiper, {style: styles.swiper, loadMinimal: true},
+                            pressed ? cards[0] : cards)
+      }
+    </SafeAreaView>
   );
 }
 
@@ -15,7 +31,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  button: {
+    flex: 1,
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  card: {
+    flex: 0,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').width,
+    borderWidth: 3,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  swiper: {
+    flex: 0,
+    height: Dimensions.get('window').width + 50,
+  }
 });
